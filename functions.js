6e8c2c27c054913ff064b2168fc8c9a8f48a130c
baldevsiyol1234-design <baldevsiyol1234@gmail.com>
2025-09-19 21:08:45 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,62 +1,65 @@
-
-
-// variables
+// 🔹 Handle resize refresh
 var $win = $(window);
 var clientWidth = $win.width();
 var clientHeight = $win.height();
 
-$(window).resize(function() {
-    var newWidth = $win.width();
-    var newHeight = $win.height();
-    if (newWidth != clientWidth && newHeight != clientHeight) {
-        location.replace(location);
-    }
+$(window).resize(function () {
+  var newWidth = $win.width();
+  var newHeight = $win.height();
+  if (newWidth !== clientWidth && newHeight !== clientHeight) {
+    location.reload();
+  }
 });
 
-(function($) {
-	$.fn.typewriter = function() {
-		this.each(function() {
-			var $ele = $(this), str = $ele.html(), progress = 0;
-			$ele.html('');
-			var timer = setInterval(function() {
-				var current = str.substr(progress, 1);
-				if (current == '<') {
-					progress = str.indexOf('>', progress) + 1;
-				} else {
-					progress++;
-				}
-				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
-				if (progress >= str.length) {
-					clearInterval(timer);
-				}
-			}, 75);
-		});
-		return this;
-	};
+// 🔹 Typewriter effect
+(function ($) {
+  $.fn.typewriter = function () {
+    this.each(function () {
+      var $ele = $(this),
+        str = $ele.html(),
+        progress = 0;
+      $ele.html('');
+      var timer = setInterval(function () {
+        var current = str.substr(progress, 1);
+        if (current === '<') {
+          progress = str.indexOf('>', progress) + 1;
+        } else {
+          progress++;
+        }
+        $ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+        if (progress >= str.length) {
+          clearInterval(timer);
+        }
+      }, 75);
+    });
+    return this;
+  };
 })(jQuery);
 
-function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes; 
-	$("#clock").html(result);
+// 🔹 Countdown function
+function startClock(targetDate) {
+  function updateClock() {
+    var now = new Date();
+    var diff = new Date(targetDate) - now;
 
-	var text = "THE WORLD JUST GOT LUCKIER SINCE ";
-	$("#message-box").html(text);
+    if (diff <= 0) {
+      $("#clock").html("🎂 Today is your Birthday! 🎉❤️");
+      return;
+    }
 
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    var minutes = Math.floor((diff / (1000 * 60)) % 60);
+    var seconds = Math.floor((diff / 1000) % 60);
+
+    $("#clock").html(
+      "Days <span class='digit'>" + days + "</span> " +
+      "Hours <span class='digit'>" + hours + "</span> " +
+      "Minutes <span class='digit'>" + minutes + "</span> " +
+      "Seconds <span class='digit'>" + seconds + "</span>"
+    );
+  }
+
+  updateClock();
+  setInterval(updateClock, 1000);
 }
